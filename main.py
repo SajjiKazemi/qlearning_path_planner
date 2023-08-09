36ea167aafe6592faee849923b0ebf92031c9a86
sajji <sajjadkazemi.e@gmail.com>
2023-08-09 19:15:22 -0400

    Add the necessities to run the q-learning on CybeGym.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+import gym
+import numpy as np
+
+from src.RL import RL
+
 import sys
 sys.path.append("./cube_gym")
-
-import gym
 from cube_gym.envs import CubeGym
 
-env = gym.make("cube_gym/CubeGym-v0", render_mode="human", size=5)
+
+env = gym.make("cube_gym/CubeGym-v0", size=9)
 observation, info = env.reset()
+model = RL(env, env.nStates, env.nActions, discount=0.9)
 
-for _ in range(10000):
-    action = env.action_space.sample()
-    observation, reward, terminated, x, y = env.step(action)
+[Q, policy] = model.qLearning(current_state=info["current_state"], initialQ=np.zeros([env.nActions, env.nStates]),
+                nEpisodes=100000, nSteps=100000, epsilon=0.5)
 
-    if terminated:
-        observation, info = env.reset()
-
-env.close()
+reward, path = model.run_model(Q, info["current_state"])
+print(reward)
+legal_moves = {0: 'right', 1: 'up', 2: 'left', 3: 'down', 4: 'forward'}
+with open('qlearning' + f"{0.5}" + '.txt', 'w') as f:
+    for item in path:
+        f.write("%s" % str(item) + '    ' + '---->' + '     ' + legal_moves[item[1]] + '\n')
+print("\nQ-learning results")
--- src/RL.py
+++ src/RL.py
@@ -1,40 +1,22 @@
 import numpy as np
-import MDP
+import gym
+import time
 
 class RL:
-    def __init__(self,mdp,sampleReward):
+    def __init__(self, env: gym, nStates: int, nActions: int, discount=0.9):
         '''Constructor for the RL class
 
         Inputs:
-        mdp -- Markov decision process (T, R, discount)
-        sampleReward -- Function to sample rewards (e.g., bernoulli, Gaussian).
         This function takes one argument: the mean of the distributon and 
         returns a sample from the distribution.
         '''
+        self.env = env
+        self.discount = discount
+        self.nStates = nStates
+        self.nActions = nActions
 
-        self.mdp = mdp
-        self.sampleReward = sampleReward
 
-    def sampleRewardAndNextState(self,state,action):
-        '''Procedure to sample a reward and the next state
-        reward ~ Pr(r)
-        nextState ~ Pr(s'|s,a)
-
-        Inputs:
-        state -- current state
-        action -- action to be executed
-
-        Outputs: 
-        reward -- sampled reward
-        nextState -- sampled next state
-        '''
-
-        reward = self.sampleReward(self.mdp.R[action,state])
-        cumProb = np.cumsum(self.mdp.T[action,state,:])
-        nextState = np.where(cumProb >= np.random.rand(1))[0][0]
-        return [reward,nextState]
-
-    def qLearning(self,s0,initialQ,nEpisodes,nSteps,epsilon=0,temperature=0):
+    def qLearning(self, current_state, initialQ, nEpisodes, nSteps, epsilon=0):
         '''qLearning algorithm.  Epsilon exploration and Boltzmann exploration
         are combined in one procedure by sampling a random action with 
         probabilty epsilon and performing Boltzmann exploration otherwise.  
@@ -55,32 +37,64 @@ class RL:
 
         # temporary values to ensure that the code compiles until this
         # function is coded
+        initialQ = np.zeros([self.nActions, self.nStates])
         Q = initialQ
+        current_state = current_state
         reward_list = np.zeros(nEpisodes)
-        visit_numbers = np.zeros([self.mdp.nActions, self.mdp.nStates], dtype=float)
-        for i in range(100):
+        visit_numbers = np.zeros([self.nActions, self.nStates], dtype=float)
+        _ , info = self.env.reset(seed=np.random.randint(0, 100000))
+        for i in range(1):
             cum_reward = np.zeros(0)
             Q = initialQ
-            visit_numbers = np.zeros([self.mdp.nActions, self.mdp.nStates], dtype=float)
+            visit_numbers = np.zeros([self.nActions, self.nStates], dtype=float)
             for n in range(nEpisodes):
                 total_reward = 0
                 for t in range(nSteps):
                     if np.random.rand(1) < epsilon:
-                        action = np.random.randint(0, self.mdp.nActions)
+                        action = np.random.randint(0, self.nActions)
                     else:
-                        action = np.argmax(Q[:, s0])
-                    reward, nextState = self.sampleRewardAndNextState(s0, action)
-                    visit_numbers[action, s0] = visit_numbers[action, s0] + 1
-                    learning_rate = 1 / visit_numbers[action, s0]
-                    Q[action, s0] = Q[action, s0] + learning_rate * (reward + self.mdp.discount * np.max(Q[:, nextState]) - Q[action, s0])
-                    s0 = nextState
-                    total_reward = total_reward + (self.mdp.discount**(t))*reward
+                        action = np.argmax(Q[:, current_state-1])
+
+                    observation, reward, terminated, reached_goal, info = self.env.step(action)
+
+                    next_state = info['next_state']
+                    visit_numbers[action, current_state-1] = visit_numbers[action, current_state-1] + 1
+                    learning_rate = 1 / visit_numbers[action, current_state-1]
+                    Q[action, current_state-1] = Q[action, current_state-1] + learning_rate *\
+                        (reward + self.discount * np.max(Q[:, next_state-1]) - Q[action, current_state-1])
+                    current_state1 = current_state
+                    current_state = next_state
+                    total_reward = total_reward + (self.discount**(t))*reward
+                    
+                    if terminated:
+                      observation, info = self.env.reset(seed=np.random.randint(0, 100000))
+                      t = nSteps
+                      current_state = info['current_state']
+                      break
                 cum_reward = np.append(cum_reward,total_reward)
             reward_list = reward_list + cum_reward
 
         policy = np.argmax(Q, axis=0)
-        average_reward = reward_list / 100
-        with open('qlearning' + f"{epsilon}" + '.txt', 'w') as f:
-            for item in average_reward:
-                f.write("%s\n" % item)
-        return [Q,policy]    
+        #average_reward = reward_list / 100
+        #with open('qlearning' + f"{epsilon}" + '.txt', 'w') as f:
+        #    for item in average_reward:
+        #        f.write("%s\n" % item)
+        return [Q,policy]
+    
+    def run_model(self, Q, initial_state):
+        path = []
+        observation, info = self.env.reset(seed=np.random.randint(0, 100000))
+        current_state = info['current_state']
+        total_reward = 0
+        done = False
+        while not done:
+            action = np.argmax(Q[:, current_state-1])
+            observation, reward, terminated, done, info = self.env.step(action)
+            #self.env.render(mode='3d')
+            time.sleep(1)
+            current_state = info['next_state']
+            total_reward = total_reward + reward
+            path.append((info['current_state'], action, self.env.get_agent_location()))
+        return total_reward, path
+
+        
